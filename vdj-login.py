408e0eb70d438f16abeffea6d6ce634ced833de7
Jacquelyn <jacquelynturcinovic@gmail.com>
2015-12-03 16:30:00 -0600

    Added print statement

--- vdj-login.py
+++ vdj-login.py
@@ -3,8 +3,8 @@ import requests
 from agavepy.agave import Agave
 
 token_url = 'https://vdjserver.org:443/api/v1/token'
-username = 
-password = 
+username = ''
+password = ''
 
 auth = requests.auth.HTTPBasicAuth(username, password)
 resp = requests.post(token_url, auth = auth, verify = False)
@@ -12,3 +12,4 @@ resp.raise_for_status()
 token_info = resp.json()
 token_dict = token_info['result']
 token = token_dict['access_token'] # THIS IS ACCESS TOKEN
+print('Your token is:', token)
