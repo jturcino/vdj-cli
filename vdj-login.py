505095f41fc4c981420af9f53a9df4432b8d4f45
Jacquelyn <jacquelynturcinovic@gmail.com>
2015-12-04 15:49:05 -0600

    Implented argparse so username and password are entered when invoking vdj-login.py. The command line format is as follows: $ python vdj-login.py username password

--- vdj-login.py
+++ vdj-login.py
@@ -1,15 +1,22 @@
+#!/usr/bin/env python
+
 import json
 import requests
 from agavepy.agave import Agave
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('username')
+parser.add_argument('password')
+args = parser.parse_args()
 
 token_url = 'https://vdjserver.org:443/api/v1/token'
-username = ''
-password = ''
 
-auth = requests.auth.HTTPBasicAuth(username, password)
+auth = requests.auth.HTTPBasicAuth(args.username, args.password)
 resp = requests.post(token_url, auth = auth, verify = False)
 resp.raise_for_status()
+
 token_info = resp.json()
 token_dict = token_info['result']
 token = token_dict['access_token'] # THIS IS ACCESS TOKEN
-print('Your token is:', token)
+print 'The token for username', args.username, 'is:', str(token)
