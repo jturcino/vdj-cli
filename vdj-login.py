02e2dd0c6ec81d5956a86d67c39aab84a0751e3f
Jacquelyn <jacquelynturcinovic@gmail.com>
2015-12-04 17:16:15 -0600

    added an optional --refresh to be followed by the refresh token

--- vdj-login.py
+++ vdj-login.py
@@ -8,15 +8,23 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('username')
 parser.add_argument('password')
+parser.add_argument('--refresh')
 args = parser.parse_args()
 
 token_url = 'https://vdjserver.org:443/api/v1/token'
+if args.refresh:
+    auth = requests.auth.HTTPBasicAuth(args.username, args.refresh)
+    resp = requests.put(token_url, auth = auth, verify = False)
 
-auth = requests.auth.HTTPBasicAuth(args.username, args.password)
-resp = requests.post(token_url, auth = auth, verify = False)
+else:
+    auth = requests.auth.HTTPBasicAuth(args.username, args.password)
+    resp = requests.post(token_url, auth = auth, verify = False)
+
 resp.raise_for_status()
 
 token_info = resp.json()
 token_dict = token_info['result']
-token = token_dict['access_token'] # THIS IS ACCESS TOKEN
-print 'The token for username', args.username, 'is:', str(token)
+access_token = token_dict['access_token'] # THIS IS ACCESS TOKEN
+refresh_token = token_dict['refresh_token'] # THIS IS REFRESH TOKEN
+print 'The access token for username', args.username, 'is:', str(access_token)
+print 'The refresh token for username', args.username, 'is:', str(refresh_token)
