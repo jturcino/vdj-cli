be99cba746dd39dee78a2bd916a4e0ad39bd2ff2
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2015-12-10 17:22:12 -0600

    added functions

--- vdj-login.py
+++ vdj-login.py
@@ -5,28 +5,38 @@ import requests
 from agavepy.agave import Agave
 import argparse
 
+def parse_response(resp):
+    token_info = resp.json()
+    token_dict = token_info['result']
+    access_token = token_dict['access_token']
+    refresh_token = token_dict['refresh_token']
+    return (str(access_token), str(refresh_token))
+
+def create(token_url,username, password):
+    auth = requests.auth.HTTPBasicAuth(username, password)
+    resp = requests.post(token_url, auth = auth, verify = False)
+    resp.raise_for_status()
+    return parse_response(resp)
+
+def refresh(token_url, username, refresh_token):
+    auth = requests.auth.HTTPBasicAuth(username, refresh_token)
+    resp = requests.put(token_url, auth = auth, verify = False)
+    resp.raise_for_status()
+    return parse_response(resp)
+
 parser = argparse.ArgumentParser()
-parser.add_argument('-u','--username',dest = 'username')
-parser.add_argument('-p', '--password',dest = 'password')
-parser.add_argument('-r', '--refresh', dest = 'refresh token')
+parser.add_argument('-u','--username', dest = 'username')
+parser.add_argument('-p', '--password', dest = 'password')
+parser.add_argument('-r', '--refresh', dest = 'refresh')
 args = parser.parse_args()
 
 token_url = 'https://vdjserver.org:443/api/v1/token'
 
-# add ifs for username
-if args.refresh:
-	auth = requests.auth.HTTPBasicAuth(args.username, args.refresh)
-	resp = requests.put(token_url, auth = auth, verify = False)
+if args.refresh != None:
+    (access_token, refresh_token) = refresh(token_url, args.username, args.refresh)
 
 else:
-	auth = requests.auth.HTTPBasicAuth(args.username, args.password)
-	resp = requests.post(token_url, auth = auth, verify = False)
+    (access_token, refresh_token) = create(token_url, args.username, args.password)
 
-	resp.raise_for_status()
-
-	token_info = resp.json()
-	token_dict = token_info['result']
-	access_token = token_dict['access_token'] # THIS IS ACCESS TOKEN
-	refresh_token = token_dict['refresh_token'] # THIS IS REFRESH TOKEN
-	print 'The access token for username', args.username, 'is:', str(access_token)
-	print 'The refresh token for username', args.username, 'is:', str(refresh_token)
+print 'Access token is:', access_token
+print 'Refresh token is:', refresh_token
