41bf9df93f4fe090e0e7093ede9d8141284a9ffd
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-02-05 16:52:38 -0600

    added but did not implement publicOnly (-P) and privateOnly (-Q) flags for eventual use as endpoint parameters

--- vdj-apps-list.py
+++ vdj-apps-list.py
@@ -11,15 +11,19 @@ if __name__ == '__main__':
     # arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
+    parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None)
+#    parser.add_argument('-P', '--publiconly', dest = 'publiconly', action = INSERTHERE)
+#    parser.add_argument('-Q', '--privateonly', dest = 'privateonly', action = INSERTHERE)
     args = parser.parse_args()
 
     # get token
-    access_token = vdjpy.read_cache('access_token')
-    if access_token is None:
-        access_token = vdjpy.prompt_user('access_token')
+    if args.accesstoken is None:
+        args.accesstoken = vdjpy.read_cache('access_token')
+        if args.accesstoken is None:
+            args.accesstoken = vdjpy.prompt_user('access_token')
 
     # get systems
-    my_agave = vdjpy.make_vdj_agave(access_token)
+    my_agave = vdjpy.make_vdj_agave(args.accesstoken)
     apps = my_agave.apps.list()
 
     # if -v
