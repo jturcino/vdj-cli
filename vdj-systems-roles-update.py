4095ddc953646307e8879cb332bba2be9e4c910d
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-02-05 15:48:00 -0600

    Currently not working changed endpoint to updateRoleForUser endpoint requires body = role; body is SystemRole type and causing error verbose (-v) flag not yet implemented

--- vdj-systems-roles-update.py
+++ vdj-systems-roles-update.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import json
 import argparse
 import vdjpy
@@ -7,9 +9,10 @@ if __name__ == '__main__':
     # arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--systemID', dest = 'systemID', required = True)
+    parser.add_argument('-r', '--role', dest = 'role', required = True, default = None)
+    parser.add_argument('-u', '--username', dest = 'username', required = True, default = None)
     parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', required = False, default = None, nargs = '?')
-    parser.add_argument('-r', '--role', dest = 'role', required = True, default = None, nargs = '?')
     args = parser.parse_args()
 
     # get token
@@ -21,12 +24,13 @@ if __name__ == '__main__':
         access_token = args.accesstoken
 
     # get role
-    if args.role is not 'GUEST' or 'USER' or 'PUBLISHER' or 'ADMIN' or 'OWNER' or None:
-        print 'Enter the role to set for the user. Valid roles are GUEST, USER, PUBLISHER, ADMIN, and OWNER:',
-        args.role = raw_input('')
+    print 'Role:', args.role
+#    if args.role is not 'GUEST' or 'USER' or 'PUBLISHER' or 'ADMIN' or 'OWNER' or None:
+#        print 'Enter the role to set for the user. Valid roles are GUEST, USER, PUBLISHER, ADMIN, and OWNER:',
+#        args.role = raw_input('')
 
     my_agave = vdjpy.make_vdj_agave(access_token)
-    role_update = my_agave.systems.updateRole(systemId = args.systemID, body = args.role)
+    role_update = my_agave.systems.updateRoleForUser(systemId = args.systemID, username = args.username, body = args.role)
 
     print json.dumps(role_update, sort_keys = True, indent = 4, separators = (',', ': '))
 
