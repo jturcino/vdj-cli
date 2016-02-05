84ad2b8d12375a26896314ab28499b0a48becc37
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-02-05 15:50:10 -0600

    Currently not working changed endpoint to deleteRoleForUser, requires username (-u) and systemID (-s) raises KeyError: 'result' when attempting to hit endpoint verbose (-v) flag not implemented

--- vdj-systems-roles-delete.py
+++ vdj-systems-roles-delete.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import json
 import argparse
 import vdjpy
@@ -7,6 +9,7 @@ if __name__ == '__main__':
     # arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-s', '--systemID', dest = 'systemID', required = True)
+    parser.add_argument('-u', '--username', dest = 'username', required = True)
     parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', required = False, default = None, nargs = '?')
     args = parser.parse_args()
@@ -20,6 +23,6 @@ if __name__ == '__main__':
         access_token = args.accesstoken
 
     my_agave = vdjpy.make_vdj_agave(access_token)
-    roles_delete = my_agave.systems.deleteRoles(systemId = args.systemID)
+    role_delete = my_agave.systems.deleteRoleForUser(systemId = args.systemID, username = args.username)
     
-    print json.dumps(roles_list, sort_keys = True, indent = 4, separators = (',', ': '))
+    print json.dumps(role_delete, sort_keys = True, indent = 4, separators = (',', ': '))
