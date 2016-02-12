9673e203fb51d40aca2ddcd19333cdbcac308058
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-02-12 16:32:32 -0600

    fully working prompt added so systemID (-s), role (-r), and username (-u) flags no longer required at command line optional verbose (-v) flag now implemented optional accesstoken (-z) flag working

--- vdj-systems-roles-update.py
+++ vdj-systems-roles-update.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
 
     # arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--systemID', dest = 'systemID', required = True)
-    parser.add_argument('-r', '--role', dest = 'role', required = True, default = None)
-    parser.add_argument('-u', '--username', dest = 'username', required = True, default = None)
+    parser.add_argument('-s', '--systemID', dest = 'systemID', default = None, nargs = '?')
+    parser.add_argument('-r', '--role', dest = 'role', default = None, nargs = '?')
+    parser.add_argument('-u', '--username', dest = 'username', default = None, nargs = '?')
     parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', required = False, default = None, nargs = '?')
     args = parser.parse_args()
@@ -23,14 +23,26 @@ if __name__ == '__main__':
     else:
         access_token = args.accesstoken
 
-    # get role
-    print 'Role:', args.role
-#    if args.role is not 'GUEST' or 'USER' or 'PUBLISHER' or 'ADMIN' or 'OWNER' or None:
-#        print 'Enter the role to set for the user. Valid roles are GUEST, USER, PUBLISHER, ADMIN, and OWNER:',
-#        args.role = raw_input('')
 
+    # check systemID
+    if args.systemID is None:
+        args.systemID = vdjpy.prompt_user('system ID')
+    
+    # check username
+    if args.username is None:
+        args.username = vdjpy.prompt_user('username to add')
+
+    # check role
+    if args.role is not None:
+         args.role = args.role.upper()
+    if args.role is None or not "USER" or not "PUBLISHER" or not "ADMIN" or not "OWNER":
+        print 'Possible roles for', args.username, 'are USER, PUBLISHER, ADMIN, and OWNER. \nPlease enter the selected role:',
+        args.role = raw_input('')
+    args.role = args.role.upper()
+
+    
     my_agave = vdjpy.make_vdj_agave(access_token)
-    role_update = my_agave.systems.updateRoleForUser(systemId = args.systemID, username = args.username, body = args.role)
+    role_update = my_agave.systems.updateRoleForUser(systemId = args.systemID, username = args.username, body = {'username':args.username, 'role':args.role})
 
     print json.dumps(role_update, sort_keys = True, indent = 4, separators = (',', ': '))
 
