2ca036d197c9cfdc1d5a61e70788ee2e8a5b1d51
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-02-26 14:17:28 -0600

    all flags but offset (-o) fully implemented includes privateonly (-Q), publiconly (-P), limit (-l), accesstoken (-z), and verbose (-v)

--- vdj-apps-list.py
+++ vdj-apps-list.py
@@ -12,8 +12,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None)
-    parser.add_argument('-P', '--publiconly', dest = 'publiconly', default = '', action = 'store_true')
-    parser.add_argument('-Q', '--privateonly', dest = 'privateonly', default = '', action = 'store_true')
+    parser.add_argument('-P', '--publiconly', dest = 'publiconly', default = False, action = 'store_true')
+    parser.add_argument('-Q', '--privateonly', dest = 'privateonly', default = False, action = 'store_true')
+    parser.add_argument('-l', '--limit', dest = 'limit', default = 250, nargs = '?')
+#    parser.add_argument('-o', '--offset', dest = 'offset', default = 0, nargs = '?')
     args = parser.parse_args()
 
     # get token
@@ -21,16 +23,22 @@ if __name__ == '__main__':
         args.accesstoken = vdjpy.read_cache('access_token')
         if args.accesstoken is None:
             args.accesstoken = vdjpy.prompt_user('access_token')
+    
+    # limit/offset
+    print args.limit
+    if args.limit is None:
+        args.limit = vdjpy.prompt_user('number of apps to return')
 
     # get systems
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
 
     # public/private only
-    if args.publiconly is True:
-        args.publiconly = 'true'
-    if args.privateonly is True:
-        args.privateonly = 'true'
-    apps = my_agave.apps.list(publicOnly = args.publiconly, privateOnly = args.privateonly)
+    if args.publiconly is True and args.privateonly is False:
+        apps = my_agave.apps.list(publicOnly = args.publiconly, limit = args.limit)
+    elif args.privateonly is True and args.publiconly is False:
+        apps = my_agave.apps.list(privateOnly = 'true', limit = args.limit)
+    else:
+        apps = my_agave.apps.list(limit = args.limit)
 
     # if -v
     if args.verbose is True:
