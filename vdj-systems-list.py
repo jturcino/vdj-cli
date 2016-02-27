785c7bfba2eab5ec2eb0541efd4b2d0b260200f8
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-02-27 08:13:52 -0600

    fully working implemented limit (-l) and offset (-o) flags all other flags fully working

--- vdj-systems-list.py
+++ vdj-systems-list.py
@@ -15,6 +15,8 @@ if __name__ == '__main__':
     parser.add_argument('-D', '--default', dest = 'default', action = 'store_true')
     parser.add_argument('-Q', '--private', dest = 'private', action = 'store_true')
     parser.add_argument('-P', '--public', dest = 'public', action = 'store_true')
+    parser.add_argument('-l', '--limit', dest = 'limit', default = 250, nargs = '?')
+    parser.add_argument('-o', '--offset', dest = 'offset', default = 0, nargs = '?')
     args = parser.parse_args()
 
     kwargs = {}
@@ -34,6 +36,16 @@ if __name__ == '__main__':
         kwargs['public'] = True
     elif args.private and not args.public:
         kwargs['public'] = False
+
+    # limit
+    if args.limit is None:
+        args.limit = vdjpy.prompt_user('number of apps to return')
+    kwargs['limit'] = args.limit
+
+    # offset
+    if args.offset is None:
+        args.offset = vdjpy.prompt_user('offset value')
+    kwargs['offset'] = args.offset
 
     # get systems
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
