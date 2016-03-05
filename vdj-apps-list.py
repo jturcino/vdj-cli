e4d1ed310b2edcc8c4b08c250ded0478620fc4f3
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-03-05 09:23:05 -0600

    fully working includes verbose (-v), accesstoken (-z), publicOnly (-P), privateOnly (-Q), limit (-l), offset (-o), name (-n), and system (-S) arguments name and system arguments implemented without endpoint all others implented with kwargs and endpoints

--- vdj-apps-list.py
+++ vdj-apps-list.py
@@ -15,30 +15,52 @@ if __name__ == '__main__':
     parser.add_argument('-P', '--publiconly', dest = 'publiconly', default = False, action = 'store_true')
     parser.add_argument('-Q', '--privateonly', dest = 'privateonly', default = False, action = 'store_true')
     parser.add_argument('-l', '--limit', dest = 'limit', default = 250, nargs = '?')
-#    parser.add_argument('-o', '--offset', dest = 'offset', default = 0, nargs = '?')
+    parser.add_argument('-o', '--offset', dest = 'offset', default = 0, nargs = '?')
+    parser.add_argument('-n', '--name', dest = 'name', default = '', nargs = '?')
+    parser.add_argument('-S', '--system', dest = 'system', default = '', nargs = '?')
     args = parser.parse_args()
 
-    # get token
-    if args.accesstoken is None:
-        args.accesstoken = vdjpy.read_cache('access_token')
-        if args.accesstoken is None:
-            args.accesstoken = vdjpy.prompt_user('access_token')
-    
-    # limit/offset
-    print args.limit
+    kwargs = {}
+
+    # public/private
+    if args.publiconly is True and args.privateonly is False:
+        kwargs['publicOnly'] = args.publiconly
+    elif args.privateonly is True and args.publiconly is False:
+        kwargs['privateOnly'] = args.privateonly
+
+    # limit
     if args.limit is None:
         args.limit = vdjpy.prompt_user('number of apps to return')
+    kwargs['limit'] = args.limit
+
+    # offset
+    if args.offset is None:
+        args.offset = vdjpy.prompt_user('number to offset')
+    kwargs['offset'] = args.offset
 
     # get systems
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
+    apps = my_agave.apps.list(**kwargs)
 
-    # public/private only
-    if args.publiconly is True and args.privateonly is False:
-        apps = my_agave.apps.list(publicOnly = args.publiconly, limit = args.limit)
-    elif args.privateonly is True and args.publiconly is False:
-        apps = my_agave.apps.list(privateOnly = 'true', limit = args.limit)
-    else:
-        apps = my_agave.apps.list(limit = args.limit)
+    # name
+    if args.name is None:
+        args.name = vdjpy.prompt_user('app name')
+    if args.name:
+        filtered = []
+        for app in apps:
+            if str(app['name']) == args.name:
+                filtered.append(app)
+        apps = filtered
+
+    # system
+    if args.system is None:
+        args.system = vdjpy.prompt_user('system ID')
+    if args.system:
+        filtered = []
+        for app in apps:
+            if str(app['executionSystem']) == args.system:
+                filtered.append(app)
+        apps = filtered
 
     # if -v
     if args.verbose is True:
