362b57ba3c489d793c4d65d6e6c00367bd1019e8
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-03-24 15:29:58 -0500

    basic functionalities working

---
+++ vdj-projects-list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import vdjpy
+import argparse
+import json
+
+if __name__ == '__main__':
+
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
+    parser.add_argument('-l', '--limit', dest = 'limit', type = int, default = 5000, nargs = '?')
+    parser.add_argument('-o', '--offset', dest = 'offset', type = int, default = 0, nargs = '?')
+    parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
+    args = parser.parse_args()
+
+    # query input
+    query = '{"name":"project"}'
+
+    # make object
+    my_agave = vdjpy.make_vdj_agave(args.accesstoken)
+    projects = my_agave.meta.listMetadata(q = query, limit = args.limit, offset = args.offset)
+
+    # if -v
+    if args.verbose is True:
+        print json.dumps(projects, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
+
+    # if no -v
+    else:
+       for item in projects:
+           print item.value['name'] + '\t' + item.uuid
