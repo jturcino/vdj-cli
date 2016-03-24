2b5b17dc0eee26f59dc8cbabddcc6ba4a82e4a7d
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-03-24 15:27:12 -0500

    limit endpoint not working; offset endpoint not working

--- vdj-metadata-list.py
+++ vdj-metadata-list.py
@@ -13,6 +13,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-q', '--query', dest = 'query', type = str, default = None, nargs = '?')
     parser.add_argument('-l', '--limit', dest = 'limit', type = int, default = 5000, nargs = '?')
+    parser.add_argument('-o', '--offset', dest = 'offset', type = int, default = 0, nargs = '?')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
     args = parser.parse_args()
 
@@ -27,8 +28,13 @@ if __name__ == '__main__':
         args.limit = vdjpy.prompt_user('project limit')
         args.limit = int(args.limit)
 
+    # -o
+    if args.offset is None:
+        args.offset = vdjpy.prompt_user('offset value')
+        args.offset = int(args.offset)
+
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
-    metadata = my_agave.meta.listMetadata(q = args.query, limit = args.limit)
+    metadata = my_agave.meta.listMetadata(q = args.query, limit = args.limit, offset = args.offset)
 
     print json.dumps(metadata, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': ')) 
 
