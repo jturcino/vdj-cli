17e067aafe1a5c60a746b85536ad4751ae535a32
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-04-01 17:10:45 -0500

    fully working added limit (-l) and offset (-o) flags removed hardcoded projects_query implemented kwargs for files list metadata query implemented new functions from vdjpy

--- vdj-files-list.py
+++ vdj-files-list.py
@@ -12,30 +12,41 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--project', dest = 'project', default = None, nargs = '?')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
+    parser.add_argument('-l', '--limit', dest = 'limit', type = int, default = 5000, nargs = '?')
+    parser.add_argument('-o', '--offset', dest = 'offset', type = int, default = 0, nargs = '?')
     args = parser.parse_args()
+
+    kwargs = {}
 
     # cache and query
     projects_cache = './.vdjprojects'
-    projects_query = '{"name":"project"}'
 
     # -p
     if args.project is None:
         args.project = vdjpy.prompt_user('project name')
 
+    # -l (for listMetadata)
+    if args.limit is None:
+        args.limit = vdjpy.prompt_user('project limit')
+    kwargs['limit'] = args.limit
+
+    # -o (for listMetadata)
+    if args.offset is None:
+        args.offset = vdjpy.prompt_user('offset value')
+    kwargs['offset'] = args.offset
+
     # read cache
     uuid = None
     if os.path.isfile(os.path.expanduser(projects_cache)) is True:
-        with open(os.path.expanduser(projects_cache), 'r') as projects_file:
-            projects_list = json.load(projects_file)
-        projects_file.close()
-        uuid = vdjpy.check_for_project_name(projects_list, args.project) 
+        projects = vdjpy.read_json(projects_cache)
+        uuid = vdjpy.check_for_project_name(projects, args.project) 
 
     # make Agave object 
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
 
     # if no cache
     if uuid is None: 
-        projects = my_agave.meta.listMetadata(q = projects_query, limit = 5000)
+        projects = vdjpy.get_vdj_projects(args.accesstoken, 5000, 0) 
         uuid = vdjpy.check_for_project_name(projects, args.project) 
     
 
@@ -50,5 +61,7 @@ if __name__ == '__main__':
         uuid = str(uuid)
         files_query = '{' + '"name": { $in: ["projectFile", "projectJobFile"]}, "value.projectUuid": "' + uuid + '", "value.isDeleted": false}'
         files_query = urllib.quote(files_query)
-        files = my_agave.meta.listMetadata(q = files_query, limit = 5000)
+        kwargs['q'] = files_query
+
+        files = my_agave.meta.listMetadata(**kwargs)
         print json.dumps(files, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
