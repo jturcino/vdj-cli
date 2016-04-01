f3d0430a87ff1083483823110826032e45bdbbc3
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-04-01 16:37:56 -0500

    implemented kwargs; limit (-l) and offset (-o) not working

--- vdj-metadata-list.py
+++ vdj-metadata-list.py
@@ -17,22 +17,27 @@ if __name__ == '__main__':
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
     args = parser.parse_args()
 
+    kwargs = {}
+
     # -q
     if args.query is None:
         args.query = vdjpy.prompt_user('metadata query')
         args.query = re.sub(r'^\'|\'$', '', args.query)
     args.query = urllib.quote(args.query)
+    kwargs['q'] = args.query
 
     # -l
     if args.limit is None:
         args.limit = vdjpy.prompt_user('project limit')
+    kwargs['limit'] = args.limit
 
     # -o
     if args.offset is None:
         args.offset = vdjpy.prompt_user('offset value')
+    kwargs['offset'] = args.offset
 
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
-    metadata = my_agave.meta.listMetadata(q = args.query, limit = args.limit, offset = args.offset)
+    metadata = my_agave.meta.listMetadata(**kwargs)
 
     print json.dumps(metadata, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': ')) 
 
