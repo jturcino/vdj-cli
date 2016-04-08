9f9b190c85ffbee4f569d78d78b3f97cbdf6c784
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-04-08 15:55:36 -0500

    implemented sort (-s) for sorting output by value of key

--- vdj-projects-list.py
+++ vdj-projects-list.py
@@ -5,6 +5,11 @@ import argparse
 import json
 import os.path
 
+def sortbydatetime(jsonlist, query):
+    """Takes a list of json dictionaries with a query by which to sort the list"""
+    jsonlist = sorted(jsonlist, key = itemgetter(query))
+    return jsonlist
+
 if __name__ == '__main__':
 
     # arguments
@@ -13,6 +18,7 @@ if __name__ == '__main__':
     parser.add_argument('-l', '--limit', dest = 'limit', type = int, default = 5000, nargs = '?')
     parser.add_argument('-o', '--offset', dest = 'offset', type = int, default = 0, nargs = '?')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
+    parser.add_argument('-s', '--sort', dest = 'sort', default = '', nargs = '?')
     args = parser.parse_args()
 
     # cache
@@ -28,6 +34,10 @@ if __name__ == '__main__':
 
     # make object
     projects = vdjpy.get_vdj_projects(args.accesstoken, args.limit, args.offset)
+    
+    # if -t
+    if args.sort is not '':
+        projects = vdjpy.sortbyquery(projects, args.sort)
 
     # if -v
     if args.verbose is True:
@@ -36,7 +46,7 @@ if __name__ == '__main__':
     # if no -v
     else:
        for item in projects:
-           print item.value['name'] + '\t' + item.uuid
+           print item.value['name'] + ' ' + item.uuid
 
     # write to cache
     vdjpy.write_json(projects, projects_cache)
