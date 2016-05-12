5b096ca4d08d0f034efafa54bf0bec8d71ea4379
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-05-12 13:54:23 -0500

    Cleaned up code and added try/except for potential errors Still needs expires_at and created_at

--- vdj-login.py
+++ vdj-login.py
@@ -3,32 +3,13 @@
 import argparse
 import getpass
 import vdjpy
-import os.path
-import json
-import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning
+import requests.exceptions
+import sys
 
 cache_dict = {"apisecret":"","apikey":"","username":"","access_token":"","refresh_token":"","created_at":"","expires_in":"","expires_at":""}
 user_cache = '~/.vdjapi'
 token_url = 'https://vdjserver.org:443/api/v1/token'
 is_cache = False
-
-def read_for_login(cache, key):
-    with open(os.path.expanduser(cache), 'r') as cache_contents:
-        cache_dict = json.load(cache_contents)
-    return str(cache_dict[key])
-
-def get_token(username, refresh, password):
-    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    if password is None: # refresh token
-        auth = requests.auth.HTTPBasicAuth(username, refresh)
-        resp = requests.put(token_url, auth = auth, verify = False)
-    else: # create token
-        auth = requests.auth.HTTPBasicAuth(username, password)
-        resp = requests.post(token_url, auth = auth, verify = False)
-    resp.raise_for_status()
-    resp = resp.json()
-    return resp
 
 if __name__ == '__main__':
 
@@ -41,33 +22,34 @@ if __name__ == '__main__':
 
     # get username
     if args.username is None: # if no username given
-        if os.path.isfile(os.path.expanduser(user_cache)) is True:
-            with open(os.path.expanduser(user_cache), 'r') as contents:
-                try:
-                    cache_dict = json.load(contents)
-                    args.username = cache_dict['username']
-                    is_cache = True
-                except ValueError:
-                    print "Unexpected contents in " + user_cache
-    if args.username is unicode('') or args.username is None:  
+        cache_dict = vdjpy.read_json(user_cache)
+        try:
+            args.username = cache_dict['username']
+            is_cache = True
+        except ValueError and TypeError:
+            cache_dict = {u'username': u'', u'apikey': u'', u'access_token': u'', u'created_at': u'', 'expires_in': u'', u'apisecret': u'', u'expires_at': u'', u'refresh_token': u''} # eventually use template instead
+    if args.username is unicode('') or args.username is None:
         args.username = unicode(vdjpy.prompt_user('username'))
     print 'Username:', args.username
 
-    # if -r
-    if args.refresh is not '':
-        if args.refresh is None:
-            if is_cache is False:
-                args.refresh = vdjpy.prompt_user('refresh token')
-            else:
-                args.refresh = cache_dict['refresh_token']
+    # if -r, but no value given
+    if args.refresh is None:
+        if is_cache is False:
+            args.refresh = vdjpy.prompt_user('refresh token')
+        else:
+            args.refresh = cache_dict['refresh_token']
 
-    # if no -r
-    else:
+    # if no -r and no -p given
+    if args.refresh is '' and args.password is None:
         args.password = getpass.getpass('Enter your password: ')
 
     # get token
-    response = get_token(args.username, args.refresh, args.password)
-    
+    try:
+        response = vdjpy.get_token(args.username, args.refresh, args.password)
+    except requests.exceptions.HTTPError:
+        if args.password is None:
+            sys.exit('The refresh token is not valid. Please try again without the refresh flag.')
+
     # insert correct values
     cache_dict['username'] = args.username
     cache_dict['access_token'] = response['result']['access_token']
@@ -75,10 +57,10 @@ if __name__ == '__main__':
     cache_dict['expires_in'] = response['result']['expires_in']
     # grab time stamp from when submitted get_token for created_at
     # expires_at is created_at + expires_in
-    
+
     # write to cache
-    with open(os.path.expanduser(user_cache), 'w') as json_file:
-        json.dump(cache_dict, json_file)
+    vdjpy.write_json(cache_dict, user_cache)
 
     print 'Access token is:', str(response['result']['access_token'])
     print 'Refresh token is:', str(response['result']['refresh_token'])
+
