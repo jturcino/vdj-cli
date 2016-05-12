e60817665baf8c7b217f24756b6199de0c878e07
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-05-12 13:56:08 -0500

    removed unnecessary functions and added one new for vdj-login

--- vdjpy.py
+++ vdjpy.py
@@ -1,19 +1,23 @@
-#!/usr/bin/env python
+#!/user/bin/env python
 
 import json
 import requests
-from requests.packages.urllib3.exceptions import InsecureRequestWarning # to disable later on
 from agavepy.agave import Agave
 import os.path
 from datetime import datetime
+import argparse
+import urllib
 from operator import itemgetter
+from requests.packages.urllib3.exceptions import InsecureRequestWarning # to disable later on
 
 # global constants
 base_url = 'https://vdj-agave-api.tacc.utexas.edu'
 token_url = 'https://vdjserver.org:443/api/v1/token'
 projects_cache = './.vdjprojects'
-user_cache = '~/.vdjapi'
+user_cache = '~/.vdjapi2'
 data_url = 'data.vdjserver.org/'
+
+# does not contain parse_response, read_for_login, create, refresh, and write_tokens
 
 def check_for_project_name(json_object, name):
     """Checks for a entries with a given name in a given json dictionary"""
@@ -25,10 +29,22 @@ def get_dictionary_value(dictionary, key):
     """Returns value of given key for given dictionary"""
     return dictionary[key]
 
+def get_token(username, refresh, password):
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+    if password is None: # refresh token
+        auth = requests.auth.HTTPBasicAuth(username, refresh)
+        resp = requests.put(token_url, auth = auth, verify = False)
+    else: # create token
+        auth = requests.auth.HTTPBasicAuth(username, password)
+        resp = requests.post(token_url, auth = auth, verify = False)
+    resp.raise_for_status()
+    resp = resp.json()
+    return resp
+
 def get_uuid(project_name, accesstoken):
     uuid = None
     if os.path.isfile(os.path.expanduser(projects_cache)) is True:
-        projects = read_jsonfile(projects_cache)
+        projects = read_json(projects_cache)
         uuid = check_for_project_name(projects, project_name)
     else:
         projects = get_vdj_projects(accesstoken, 5000, 0)
@@ -57,20 +73,12 @@ def json_serial(obj):
 def make_vdj_agave(access_token):
     """Make an Agave object at url with given access token."""
     if access_token is None:
-        dictionary = read_jsonfile(user_cache)
+        dictionary = read_json(user_cache)
         if dictionary is None:
             access_token = prompt_user('access_token')
         else:
             access_token = dictionary['access_token']
     return Agave(api_server = base_url, token = access_token)
-
-def parse_response(resp):
-    """Return access and refresh tokens from JSON-generated dictionary."""
-    token_info = resp.json()
-    token_dict = get_dictionary_value(token_info, 'result')
-    access_token = get_dictionary_value(token_dict, 'access_token')
-    refresh_token = get_dictionary_value(token_dict, 'refresh_token')
-    return (str(access_token), str(refresh_token))
 
 def prompt_user(key):
     """Promp user to enter value for given key at command line."""
@@ -78,15 +86,7 @@ def prompt_user(key):
     return_key = raw_input('')
     return return_key
 
-def get_json_value(cache, key):
-    """Get the value corresponding to key. Defaults to given file, but uses input upon failure."""
-    dictionary = read_jsonfile(cache)
-    if dictionary is None:
-        return prompt_user(key)
-    else:
-        return str(dictionary[key])
-
-def read_jsonfile(filename):
+def read_json(filename):
     """Return the contents of a file containing json. Returns None if the file does not exist."""
     if os.path.isfile(os.path.expanduser(filename)) is True:
         with open(os.path.expanduser(filename), 'r') as json_file:
@@ -107,42 +107,10 @@ def sortbyquery(mylist, query):
         mylist.sort(key = itemgetter(query))
     return mylist
 
-def create(username, password):
-    """Create a new authentication token at url with given username and password."""
-    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    auth = requests.auth.HTTPBasicAuth(username, password)
-    resp = requests.post(token_url, auth = auth, verify = False)
-    resp.raise_for_status()
-    (access_token, refresh_token) = parse_response(resp)
-    if os.path.isfile(os.path.expanduser(user_cache)) is True:
-        write_tokens(access_token, refresh_token)
-    return (access_token, refresh_token)
-
-def refresh(username, refresh_token):
-    """Refresh authentication token at url with given username and refresh token."""
-    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
-    auth = requests.auth.HTTPBasicAuth(username, refresh_token)
-    resp = requests.put(token_url, auth = auth, verify = False)
-    resp.raise_for_status()
-    (access_token, refresh_token) = parse_response(resp)
-    if os.path.isfile(os.path.expanduser(user_cache)) is True:
-        write_tokens(access_token, refresh_token)
-    return (access_token, refresh_token)
-
 def write_json(json_in, filename):
     """Write the given json to the given file. Changes from unicode to string."""
     with open(os.path.expanduser(filename), 'w') as json_file:
         json.dump(json_in, json_file)
-    json_file.close()
     return
 
-def write_tokens(access_token, refresh_token):
-    """Updates access and refresh tokens before writing them to user_cache"""
-    with open(os.path.expanduser(user_cache), 'r') as json_file:
-        json_dict = json.load(json_file)
-    json_file.close()
-    json_dict['access_token'] = unicode(access_token)
-    json_dict['refresh_token'] = unicode(refresh_token)
-    write_json(json_dict, user_cache)
-    return
 
