f21b7981d348a4b726fdaadff1696d6eafba238c
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-05-14 11:43:59 -0500

    Added metadata update with new tags file_type (-y) read_direction (-r) tags (-t): tags themselves cannot be input at the command line. Use like verbose (-v).

--- vdj-files-upload.py
+++ vdj-files-upload.py
@@ -5,6 +5,7 @@ import argparse
 import json
 import os.path
 import urllib
+import requests
 
 system = 'data.vdjserver.org/'
 
@@ -16,9 +17,13 @@ if __name__ == '__main__':
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
     parser.add_argument('-f', '--file_upload', dest = 'file_upload', default = None, nargs = '?')
     parser.add_argument('-n', '--file_name', dest = 'file_name', default = None, nargs = '?')
+    parser.add_argument('-y', '--file_type', dest = 'file_type', default = '', nargs = '?')
+    parser.add_argument('-r', '--read_direction', dest = 'read_direction', default = '', nargs = '?')
+    parser.add_argument('-t', '--tags', dest = 'tags', default = False, action = 'store_true')
     parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
     args = parser.parse_args()
 
+    # UPLOAD FILE SETUP
     kwargs = {}
     kwargs['systemId'] = system
 
@@ -40,17 +45,43 @@ if __name__ == '__main__':
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
 
     # get uuid, exit if does not exist
-    uuid = vdjpy.get_uuid(args.project, args.accesstoken)
-    if uuid is None:
+    project_uuid = vdjpy.get_uuid(args.project, args.accesstoken)
+    if project_uuid is None:
         sys.exit()
-    kwargs['filePath'] = '/projects/' + uuid + '/files'
+    kwargs['filePath'] = '/projects/' + project_uuid + '/files'
 
     # upload file
     upload = my_agave.files.importData(**kwargs)
 
+    # UPDATE METADATA SETUP
+    file_uuid = str(upload['uuid'])
+    metadata_path = '/vdjZ/projects/' + project_uuid + '/files/' + args.file_name
+    
+    # -y
+    if args.file_type is None:
+        args.file_type = vdjpy.prompt_user('file type')
+
+    # -r
+    if args.read_direction is None:
+        args.read_direction = vdjpy.prompt_user('read_direction')
+
+    metadata_url = 'https://vdjserver.org/api/v1/notifications/files/import?fileUuid=' + file_uuid + '&path=' + metadata_path + '&projectUuid=' + project_uuid + '&vdjFileType=' + str(args.file_type) + '&readDirection=' + args.read_direction + '&tags='
+
+    # -t
+    if args.tags:
+        tag_string = vdjpy.prompt_user('tags as a comma-separated list')
+        tag_string = urllib.quote(tag_string)
+        metadata_url += tag_string
+
+    # update metadata
+    resp = requests.post(metadata_url)
+    resp.raise_for_status()
+    resp = resp.json()
+
     # if -v
     if args.verbose:
         print json.dumps(upload, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': ')) 
+        print json.dumps(resp, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
 
     # if no -v
     else:
