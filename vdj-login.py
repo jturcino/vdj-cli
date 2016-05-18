e9eb7bf3cd826828bb3cab71d5da4c9acc987f19
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-05-17 23:17:42 -0500

    added save (-s) flag and template loading

--- vdj-login.py
+++ vdj-login.py
@@ -7,8 +7,8 @@ import requests.exceptions
 import sys
 import time
 
-cache_dict = {"apisecret":"","apikey":"","username":"","access_token":"","refresh_token":"","created_at":"","expires_in":"","expires_at":""}
 user_cache = '~/.vdjapi'
+template_cache = './cache-template.json'
 token_url = 'https://vdjserver.org:443/api/v1/token'
 is_cache = False
 
@@ -19,26 +19,27 @@ if __name__ == '__main__':
     parser.add_argument('-u', '--username', required = False, dest = 'username', default = None, nargs = '?')
     parser.add_argument('-p', '--password', required = False, dest = 'password', default = None, nargs = '?')
     parser.add_argument('-r', '--refresh', required = False, dest = 'refresh', default = '', nargs = '?', type = str)
+    parser.add_argument('-s', '--save', required = False, dest = 'save', default = False, action = 'store_true')
     args = parser.parse_args()
 
-    # get username
-    if args.username is None: # if no username given
+    # get username and load cache if to be saved
+    if args.username is None or args.save is True:
         cache_dict = vdjpy.read_json(user_cache)
-        try:
-            args.username = cache_dict['username']
+        if cache_dict is None or cache_dict.get('username') is None or cache_dict.get('username') is unicode(''):
+            args.username = vdjpy.prompt_user('username')
+            if args.save is True:
+                cache_dict = vdjpy.read_json(template_cache)
+        else:
             is_cache = True
-        except ValueError and TypeError:
-            cache_dict = {u'username': u'', u'apikey': u'', u'access_token': u'', u'created_at': u'', 'expires_in': u'', u'apisecret': u'', u'expires_at': u'', u'refresh_token': u''} # eventually use template instead
-    if args.username is unicode('') or args.username is None:
-        args.username = unicode(vdjpy.prompt_user('username'))
+            args.username = cache_dict.get('username')
     print 'Username:', args.username
 
     # if -r, but no value given
     if args.refresh is None:
-        if is_cache is False:
-            args.refresh = vdjpy.prompt_user('refresh token')
+        if is_cache:
+            args.refresh = cache_dict.get('refresh_token')
         else:
-            args.refresh = cache_dict['refresh_token']
+            args.refresh = vdjpy.prompt_user('refresh token')
 
     # if no -r and no -p given
     if args.refresh is '' and args.password is None:
@@ -49,20 +50,19 @@ if __name__ == '__main__':
         response = vdjpy.get_token(args.username, args.refresh, args.password)
         current_time = int(time.time())
     except requests.exceptions.HTTPError:
-        if args.password is None:
-            sys.exit('The refresh token is not valid. Please try again without the refresh flag.')
+        sys.exit('The username, password, or refresh token is not valid. Please try again.')
 
-    # insert correct values
-    cache_dict['username'] = args.username
-    cache_dict['access_token'] = response['result']['access_token']
-    cache_dict['refresh_token'] = response['result']['refresh_token']
-    cache_dict['expires_in'] = response['result']['expires_in']
-    cache_dict['created_at'] = current_time
-    cache_dict['expires_at'] = time.ctime(current_time + response['result']['expires_in'])
+    # if -S, insert correct values and write to user_cache
+    if args.save:
+        cache_dict['username'] = args.username
+        cache_dict['access_token'] = response['result']['access_token']
+        cache_dict['refresh_token'] = response['result']['refresh_token']
+        cache_dict['expires_in'] = response['result']['expires_in']
+        cache_dict['created_at'] = current_time
+        cache_dict['expires_at'] = time.ctime(current_time + response['result']['expires_in'])
+        vdjpy.write_json(cache_dict, user_cache)
 
-    # write to cache
-    vdjpy.write_json(cache_dict, user_cache)
-
+    # print tokens
     print 'Access token is:', str(response['result']['access_token'])
     print 'Refresh token is:', str(response['result']['refresh_token'])
 
