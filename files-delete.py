a3cb6ced5f9ae2ed2df78c62df72f3c3475ab18b
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-05-22 15:17:49 -0500

    give path (-p) rather than project and filename

--- files-delete.py
+++ files-delete.py
@@ -9,36 +9,26 @@ if __name__ == '__main__':
 
     # arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--project', dest = 'project', default = None, nargs = '?')
+    parser.add_argument('-p', '--path', dest = 'path', default = None, nargs = '?')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
-    parser.add_argument('-f', '--file_to_delete', dest = 'file_to_delete', default = None, nargs = '?')
     parser.add_argument('-s', '--system', dest = 'system', default = None, nargs = '?')
     args = parser.parse_args()
 
     kwargs = {}
 
+    # -s
     if args.system is None:
         args.system = vdjpy.prompt_user('system name')
     kwargs['systemId'] = args.system
 
     # -p
-    if args.project is None:
-        args.project = vdjpy.prompt_user('project name')
-    project_uuid = vdjpy.get_uuid(args.project, args.accesstoken)
-    if project_uuid is None:
-        sys.exit('Could not find specified project.')
-    project_uuid = str(project_uuid)
-    
-    # -f
-    if args.file_to_delete is None:
-        args.file_to_delete = vdjpy.prompt_user('file to delete')
-    
-    # construct path
-    kwargs['filePath'] = '/projects/' + project_uuid + '/files/' + args.file_to_delete
+    if args.path is None:
+        args.path = vdjpy.prompt_user('path to the file')
+    kwargs['filePath'] = args.path
 
     # delete file
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
     delete = my_agave.files.delete(**kwargs)
 
     if delete is None:
-        print 'Successfully deleted', args.file_to_delete, 'at', kwargs['filePath']
+        print 'Successfully deleted file at path', args.path
