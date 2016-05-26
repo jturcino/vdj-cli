0e920b020022e89020f5983a0b77e037b4c56816
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-05-26 16:23:53 -0500

    not working; issue with body for endpoint

---
+++ files-move.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+import vdjpy
+import json
+import argparse
+
+if __name__ == '__main__':
+    
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--system', dest = 'system', default = None, nargs = '?')
+    parser.add_argument('-p', '--path', dest = 'path', default = None, nargs = '?')
+    parser.add_argument('-d', '--destination', dest = 'destination', default = None, nargs = '?')
+    parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
+    parser.add_argument('-v', '--verbose', dest = 'verbose', default = False, action = 'store_true')
+    args = parser.parse_args()
+
+    # -s
+    if args.system is None:
+        args.system = vdjpy.prompt_user('system name')
+
+    # -p
+    if args.path is None:
+        args.path = vdjpy.prompt_user('path to the file')
+
+    # -d
+    if args.destination is None:
+        args.destination = vdjpy.prompt_user('destination of the file')
+    data_change = "{\"action\":\"move\",\"newPath\": \"" + args.destination + "\"}"
+
+    # copy file
+    move = vdjpy.manage_files(args.accesstoken, args.system, args.path, data_change)
+
+    # if -v
+    if args.verbose:
+         print json.dumps(move, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
+
+    # if no -v
+    else:
+        print 'Now moving', str(move['name']), 'from', args.path, 'to', args.destination 
