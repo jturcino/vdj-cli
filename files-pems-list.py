00f3b414c0bed3efa3e188265c16f13dc4d083b7
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-06-09 09:23:30 -0500

    added offset (-o) and limit (-l) flags

--- files-pems-list.py
+++ files-pems-list.py
@@ -10,6 +10,8 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path', dest = 'path', default = None, nargs = '?')
     parser.add_argument('-s', '--systemID', dest = 'systemID', default = None, nargs = '?')
+    parser.add_argument('-l', '--limit', dest = 'limit', default = 250, type = int, nargs = '?')
+    parser.add_argument('-o', '--offser', dest = 'offset', default = 0, type = int, nargs = '?')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
     parser.add_argument('-v', '--verbose', dest = 'verbose', default = False, action = 'store_true')
     args = parser.parse_args()
@@ -25,6 +27,24 @@ if __name__ == '__main__':
     if args.path is None:
         args.path = vdjpy.prompt_user('path')
     kwargs['filePath'] = args.path
+
+    # -l
+    if args.limit is None:
+        try:
+            args.limit = int(vdjpy.prompt_user('limit'))
+        except:
+            print 'Not a valid integer. Using default value of 250.'
+            args.limit = 250
+    kwargs['limit'] = args.limit
+
+    # -o
+    if args.offset is None:
+        try:
+            args.offset = int(vdjpy.prompt_user('offset value'))
+        except:
+            print 'Not a valid integer. Using default value of 0.'
+            args.offset = 0
+    kwargs['offset'] = args.offset
 
     # list permissions
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
