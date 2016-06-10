4116048c9d534d649811f6b8283d1ce47d6b4337
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-06-10 12:09:36 -0500

    initial version uses path (-p) and systemID (-s) includes accesstoken (-z), but does not have verbose (-v) because returned response is None

---
+++ files-pems-delete.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import vdjpy
+import json
+import argparse
+
+if __name__ == '__main__':
+    
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--path', dest = 'path', default = None, nargs = '?')
+    parser.add_argument('-s', '--systemID', dest = 'systemID', default = None, nargs = '?')
+    parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
+    args = parser.parse_args()
+
+    kwargs = {}
+
+    # -s
+    if args.systemID is None:
+        args.systemID = vdjpy.prompt_user('system')
+    kwargs['systemId'] = args.systemID
+
+    # -p
+    if args.path is None:
+        args.path = vdjpy.prompt_user('path')
+    kwargs['filePath'] = args.path
+
+    # delete permissions
+    my_agave = vdjpy.make_vdj_agave(args.accesstoken)
+    pems_delete = my_agave.files.deletePermissions(**kwargs)
+
+    if pems_delete is None:
+        print 'Succesfully removed permissions for file at path', args.path
+    else:
+        print 'Permission removal was not successful. Check listed permissions, and try again.'
