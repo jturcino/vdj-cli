50b1215866a3d5be782cb1f4f0b8b2fa01c72220
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-06-10 16:40:52 -0500

    changed endpoint to files.downloadFromDefaultSystem

--- vdj-files-get.py
+++ vdj-files-get.py
@@ -5,7 +5,6 @@ import argparse
 import os.path
 import sys
 
-system = 'data.vdjserver.org/'
 
 if __name__ == '__main__':
 
@@ -13,38 +12,31 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
     parser.add_argument('-n', '--file_name', dest = 'file_name', default = None, nargs = '?')
-#    parser.add_argument('-r', '--recursive', dest = 'recursive')
     parser.add_argument('-f', '--file_download', dest = 'file_download', default = None, nargs = '?')
     parser.add_argument('-p', '--project', dest = 'project', default = None, nargs = '?')
     args = parser.parse_args()
     
     kwargs = {}
-    kwargs['systemId'] = system
 
     # -p
     if args.project is None:
         args.project = vdjpy.prompt_user('project name')
+    project_uuid = vdjpy.get_uuid(args.project, args.accesstoken)
+    if project_uuid is None:
+        sys.exit('Cancelling command because of invalid project name')
 
     # -f
     if args.file_download is None:
         args.file_download = vdjpy.prompt_user('file to download')
+    kwargs['sourcefilePath'] = '/projects/' + project_uuid + '/files/' + args.file_download
 
     # -n
     if args.file_name is None:
         args.file_name = args.file_download
 
-    # get project uuid; exit if does not exist
-    project_uuid = vdjpy.get_uuid(args.project, args.accesstoken)
-    if project_uuid is None:
-        sys.exit('Cancelling command because of invalid project name')
-
-    # build path
-    path = '/projects/' + project_uuid + '/files/' + args.file_download
-    kwargs['filePath'] = path
-
     # make Agave object and download file
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
-    download = my_agave.files.download(**kwargs)
+    download = my_agave.files.downloadFromDefaultSystem(**kwargs)
     download.raise_for_status()
 
     # write correct contents to file
