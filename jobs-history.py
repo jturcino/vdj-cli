75b3bfba6390147a5b8330fe496e5c3d6937d0bd
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-06-13 15:04:31 -0500

    uses jobID (-j), limit (-l), offset (-o), and accesstoken (-z). Does not use verbose (-v).

---
+++ jobs-history.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import vdjpy
+import json
+import argparse
+import sys
+
+if __name__ == '__main__':
+    
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-j', '--jobID', dest = 'jobID', default = None, nargs = '?')
+    parser.add_argument('-l', '--limit', dest = 'limit', type = int, default = 250, nargs = '?')
+    parser.add_argument('-o', '--offset', dest = 'offset', type = int, default = 0, nargs = '?')
+    parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
+    args = parser.parse_args()
+    
+    kwargs = {}
+
+    # -j
+    if args.jobID is None:
+        args.jobID = vdjpy.prompt_user('jobID')
+    kwargs['jobId'] = args.jobID
+
+    # -l
+    if args.limit is None:
+        args.limit = vdjpy.prompt_for_integer('limit', 250)
+    kwargs['limit'] = args.limit
+
+    # -o
+    if args.offset is None:
+        args.offset = vdjpy.prompt_for_integer('offset', 0)
+    kwargs['offset'] = args.offset
+
+    # get history
+    my_agave = vdjpy.make_vdj_agave(args.accesstoken)
+    history = my_agave.jobs.getHistory(**kwargs)
+    print json.dumps(history, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
