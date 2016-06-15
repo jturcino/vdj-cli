e51e8fc6e048fa4b09a6e4d8729caaf5eae1c400
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-06-15 09:50:19 -0500

    takes jobID (-j); verbose (-v) and accesstoken (-z) also available

---
+++ jobs-status.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import vdjpy
+import json
+import argparse
+import sys
+
+if __name__ == '__main__':
+    
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-j', '--jobID', dest = 'jobID', default = None, nargs = '?')
+    parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
+    parser.add_argument('-v', '--verbose', dest = 'verbose', default = False, action = 'store_true')
+    args = parser.parse_args()
+    
+    kwargs = {}
+
+    # -j
+    if args.jobID is None:
+        args.jobID = vdjpy.prompt_user('jobID')
+    kwargs['jobId'] = args.jobID
+
+    # get status 
+    my_agave = vdjpy.make_vdj_agave(args.accesstoken)
+    status = my_agave.jobs.getStatus(**kwargs)
+
+    # -v
+    if args.verbose:
+        print json.dumps(status, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
+
+    # if no -v
+    else:
+        print 'The status of job',  args.jobID, 'is', status['status']
