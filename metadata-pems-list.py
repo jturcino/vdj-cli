f4832c1347d54b1f2fb59202a2efc04adc7177b7
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-06-16 09:50:01 -0500

    limit, offset not working properly limit not working at all offset working like limit

---
+++ metadata-pems-list.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import vdjpy
+import json
+import argparse
+
+if __name__ == '__main__':
+
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--uuid', dest = 'uuid', default = None, nargs = '?')
+    parser.add_argument('-l', '--limit', dest = 'limit', type = int, default = 250, nargs = '?')
+    parser.add_argument('-o', '--offset', dest = 'offset', type = int, default = 0, nargs = '?')
+    parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
+    parser.add_argument('-v', '--verbose', dest = 'verbose', default = False, action = 'store_true')
+    args = parser.parse_args()
+
+    kwargs = {}
+
+    # -u
+    if args.uuid is None:
+        args.uuid = vdjpy.prompt_user('uuid of item')
+    kwargs['uuid'] = args.uuid
+
+    # -l
+    if args.limit is None:
+        args.limit = vdjpy.prompt_for_integer('limit', 250)
+    kwargs['limit'] = args.limit
+
+    # -o
+    if args.offset is None:
+        args.offset = vdjpy.prompt_for_integer('offset', 0)
+    kwargs['offset'] = args.offset
+
+    # list permissions
+    my_agave = vdjpy.make_vdj_agave(args.accesstoken)
+    pems = my_agave.meta.listMetadataPermissions(**kwargs)
+
+    # -v
+    if args.verbose:
+        print json.dumps(pems, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
+
+    # if no -v
+    else:
+        for item in pems:
+            print item['username'], '\n\t read:', item['permission']['read'], '\n\twrite:', item['permission']['write']
