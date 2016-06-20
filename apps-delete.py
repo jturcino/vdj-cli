07e897e888298bbb7d4bf0125f7abf8b89f86419
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-06-20 09:21:20 -0500

    ERROR WITH RESP; fix in place currently cannot use verbose (-v) uses appID (-a); verbose (-v) and accesstoken (-z) optional

---
+++ apps-delete.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import argparse
+import json
+import os.path
+import vdjpy
+
+if __name__ == '__main__':
+    
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--appID', dest = 'appID', default = None)
+#    parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
+    parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None)
+    args = parser.parse_args()
+
+    # make agave object and kwargs
+    my_agave = vdjpy.make_vdj_agave(args.accesstoken)
+    kwargs = {}
+
+    # -f
+    if args.appID is None:
+        args.appID = vdjpy.prompt_user('app ID')
+    kwargs['appId'] = args.appID
+    
+    # ERROR, NO JSON RETURNED; TEMPORARY FIX
+    # publish app
+    try:
+        delete = my_agave.apps.delete(**kwargs)
+    except ValueError:
+        print 'Successfully deleted', args.appID
+
