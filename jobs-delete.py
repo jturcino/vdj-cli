8e94ad5e277480aeddf675bc77c13247097d28ea
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-06-22 13:01:07 -0500

    takes jobID (-j); accesstoken (-z) optional

---
+++ jobs-delete.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import vdjpy
+import json
+import argparse
+import sys
+
+if __name__ == '__main__':
+    
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-j', '--jobID', dest = 'jobID', default = None, nargs = '?')
+    parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
+    args = parser.parse_args()
+    
+    kwargs = {}
+
+    # -j
+    if args.jobID is None:
+        args.jobID = vdjpy.prompt_user('jobID')
+    kwargs['jobId'] = args.jobID
+
+    # get status 
+    my_agave = vdjpy.make_vdj_agave(args.accesstoken)
+    job_delete = my_agave.jobs.delete(**kwargs)
+
+    # deliver message
+    if job_delete is None:
+        print 'Successfully deleted job', args.jobID
+    else:
+        print 'Deletion failed. Message returned from request is:\n' + json.dumps(job_delete, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
