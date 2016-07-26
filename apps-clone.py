221a60c815c874029f97777bb315a0161ac46504
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-07-26 11:59:16 -0500

    NOT WORKING ipython: HTTPError: 400 Client Error: Bad Request agave equivalent: Failed to publish app. Failed to authenticate to the remote system, jturcino-stampede-storage

--- apps-clone.py
+++ apps-clone.py
@@ -11,12 +11,12 @@ if __name__ == '__main__':
     # arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', '--appID', dest = 'appID', default = None, nargs = '?')
-    parser.add_argument('-n', '--clone_name', dest = 'clone_name', default = None, nargs = '?')
+    parser.add_argument('-n', '--clone_name', dest = 'clone_name', default = '', nargs = '?')
     parser.add_argument('-s', '--storage_system', dest = 'storage_system', default = None, nargs = '?')
-    parser.add_argument('-e', '--execution_system', dest = 'execution_system', default = None, nargs = '?')
-    parser.add_argument('-x', '--version', dest = 'version', default = None, nargs = '?')
+    parser.add_argument('-e', '--execution_system', dest = 'execution_system', default = '', nargs = '?')
+    parser.add_argument('-x', '--version', dest = 'version', default = '', nargs = '?')
     parser.add_argument('-d', '--deployment_path', dest = 'deployment_path', default = None, nargs = '?')
-#    parser.add_argument('-f', '--description_file', dest = 'description_file', default = None)
+    parser.add_argument('-f', '--description_file', dest = 'description_file', default = '', nargs = '?')
     parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None)
     args = parser.parse_args()
@@ -25,29 +25,70 @@ if __name__ == '__main__':
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
     kwargs = {}
 
-   # -a
-   if args.appID is None:
-        args.appID = vdjpy.prompt_user('ID of app to be cloned)
-   kwargs['appId'] = args.appID
+    # -a
+    if args.appID is None:
+        args.appID = vdjpy.prompt_user('ID of app to be cloned')
+    kwargs['appId'] = args.appID
 
-#############################################
-# if -f, use
-# else, use inputs
-    # if -n, -s,  -e, -x, or -d not given
-        # load app info
-        # use app info to populate args
-    # build body
-############################################
+    # -f used if given
+    if args.description_file is not '':
+        if args.description_file is None:
+            args.description_file = vdjpy.prompt_user('file name')
+        body_contents = vdjpy.read_json(args.description_file)
+        if body_contents is None:
+            sys.exit()
+        kwargs['body'] = body_contents #DOES IT NEED TO BE STRING???
 
-    # -f
-    if args.description_file is None:
-        args.description_file = vdjpy.prompt_user('path to file containing app description')
-    
-    # open -f and use as body
-    body_contents = vdjpy.read_json(args.description_file)
-    if body_contents is None:
-        sys.exit('Not a valid file path or does not contain a valid app description.')
-    kwargs['body'] = json.dumps(body_contents)
+    # -n, -s, -e, -x, and -d used if no -f
+    else:
+        # if -n, -e, or -x not given, load app data (exit if app does not exist)
+        if args.clone_name or args.execution_system or args.version is '':
+            apps = my_agave.apps.list()
+            app_info = None
+            for app in apps:
+                if app['id'] == args.appID:
+                    app_info = app
+            if app_info is None:
+                sys.exit('Could not find given app in your apps')
+        # -n
+        if args.clone_name is '':
+            vdjapi_contents = vdjpy.read_json('~/.vdjapi')
+            try:
+                username = vdjapi_contents.get('username')
+            except:
+                sys.exit('Unable to get username from ~/.vdjapi')
+            args.clone_name = app_info['name'] + '-' + username
+        elif args.clone_name is None:
+            args.clone_name = vdjpy.prompt_user('name of cloned app')
+
+        # -e
+        if args.execution_system is '':
+            args.execution_system = app_info['executionSystem']
+        elif args.execution_system is None:
+            args.execution_system = vdjpy.prompt_user('execution system')
+
+        # -x
+        if args.version is '':
+            args.version = app_info['version']
+        elif args.version is None:
+            args.version = vdjpy.prompt_user('version of cloned app')
+
+        # -s
+        if args.storage_system is None:
+            args.storage_system = vdjpy.prompt_user('storage system')
+
+        # -d
+        if args.deployment_path is None:
+            args.deployment_path = vdjpy.prompt_user('deployment path')
+
+
+        # build body
+        kwargs['body'] = {'action': 'clone',
+			  'name': args.clone_name,
+			  'storageSystem': args.storage_system,
+			  'executionSystem': args.execution_system,
+			  'version': args.version,
+			  'deploymentPath': args.deployment_path}
 
     # clone app
     clone = my_agave.apps.manage(**kwargs)
@@ -58,4 +99,4 @@ if __name__ == '__main__':
 
     # if no -v
     else:
-        print 'Successfully added app', clone['id']
+        print 'Successfully cloned app', clone['id']
