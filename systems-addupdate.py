3c52604fe7f2a39ed9986fd3b77235f8a610dd45
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-08-03 15:58:39 -0500

    updates system if -s given; adds otherwise

--- systems-addupdate.py
+++ systems-addupdate.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     
     # arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a', '--systemID', dest = 'systemID', default = None)
+    parser.add_argument('-s', '--systemID', dest = 'systemID', default = '')
     parser.add_argument('-f', '--description_file', dest = 'description_file', default = None)
     parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None)
@@ -19,11 +19,6 @@ if __name__ == '__main__':
     # make agave object and kwargs
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
     kwargs = {}
-
-    # -a
-    if args.systemID is None:
-        args.systemID = vdjpy.prompt_user('system ID')
-    kwargs['systemId'] = args.systemID
 
     # -f
     if args.description_file is None:
@@ -35,13 +30,23 @@ if __name__ == '__main__':
         sys.exit('Not a valid file path or does not contain a valid system description.')
     kwargs['body'] = json.dumps(body_contents)
 
-    # update system
-    update = my_agave.systems.update(**kwargs)
+    # if -s given, update system
+    if args.systemID is not '':
+        if args.systemID is None:
+	    args.systemID = vdjpy.prompt_user('system ID')
+	kwargs['systemId'] = args.systemID
+	resp = my_agave.systems.update(**kwargs)
+
+    # else, add system
+    else:
+	resp = my_agave.systems.add(**kwargs)
 
     # if -v
     if args.verbose is True:
-        print json.dumps(update, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
+        print json.dumps(resp, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
 
     # if no -v
-    else:
+    elif args.systemID is not '':
         print 'Successfully updated system', args.systemID
+    else:
+	print 'Successfully added system', resp['id']
