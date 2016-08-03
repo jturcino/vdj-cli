dff22dd3d50ab4adf43af8977713c948f22c4604
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-08-03 14:47:30 -0500

    CANNOT TEST DUE TO PEMS RESTRICTION

--- systems-unpublish.py
+++ systems-unpublish.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
         args.systemID = vdjpy.prompt_user('system ID')
     kwargs['systemId'] = args.systemID
 
-    # publish system
+    # unpublish system
     unpublish = my_agave.systems.manage(**kwargs)
 
     # if -v
