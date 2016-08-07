905f060356ecb338b4344c3bfc8e4c92e6f0ebea
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-08-07 15:43:07 -0500

    NEED TO TEST JOBFILE

--- vdj-files-rename.py
+++ vdj-files-rename.py
@@ -10,7 +10,8 @@ if __name__ == '__main__':
     # arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--project', dest = 'project', default = None, nargs = '?')
-    parser.add_argument('-f', '--file_name', dest = 'file_name', default = None, nargs = '?')
+    parser.add_argument('-f', '--file_name', dest = 'file_name', default = '', nargs = '?')
+    parser.add_argument('-j', '--jobfile_name', dest = 'jobfile_name', default = '', nargs = '?')
     parser.add_argument('-n', '--new_name', dest = 'new_name', default = None, nargs = '?')
     parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
     parser.add_argument('-v', '--verbose', dest = 'verbose', default = False, action = 'store_true')
@@ -19,10 +20,6 @@ if __name__ == '__main__':
     # make agave object 
     my_agave = vdjpy.make_vdj_agave(args.accesstoken)
 
-    # -f
-    if args.file_name is None:
-        args.file_name = vdjpy.prompt_user('current file name')
-
     # -p
     if args.project is None:
         args.project = vdjpy.prompt_user('project')
@@ -30,30 +27,42 @@ if __name__ == '__main__':
     if uuid is None:
         sys.exit()
 
+    # -f (default)
+    if args.file_name is not '' or args.jobfile_name is '':
+	if args.file_name is None:
+            args.file_name = vdjpy.prompt_user('current file name')
+	filetype = 'projectFile'
+	jobfile_boolean = False
+    # -j (only used if specified)
+    else:
+	if args.jobfile_name is None:
+	    args.jobfile_name = vdjpy.prompt_user('current jobfile name')
+	filetype = 'projectJobFile'
+	jobfile_boolean = True
+	# consolidate file name to args.file_name
+	args.file_name = args.jobfile_name
+
     # -d
     if args.new_name is None:
         args.new_name = vdjpy.prompt_user('new name')
 
-    # get metadata for file
-    project_files = vdjpy.get_project_files(uuid, {}, my_agave)
-    file_metadata = None
-    for item in project_files:
-        if item['value']['name'] == args.file_name:
-            file_metadata = item
-    
-    # if file metadata not found, exit
+    # get metadata for file; exit if not found
+    project_files = vdjpy.get_project_files(uuid, filetype, {}, my_agave)
+    file_metadata = vdjpy.get_file_metadata(project_files, args.file_name)
     if file_metadata is None:
-        print 'The file', args.file_name, 'does not exist in project', args.project + '. \nHere are the files currently in the project:'
-        for item in project_files:
-            print item['value']['name']
         sys.exit()
 
-    # change value.name to desired name if file found
+    # if jobfile, get extra path
+    extra_path = ''
+    if jobfile_boolean:
+        extra_path += str(file_metadata['value']['relativeArchivePath']) + '/'
+
+    # change value.name to desired name
     file_metadata['value']['name'] = unicode(args.new_name)
 
     # rename in agave and via metadata update
     agave_rename = my_agave.files.manage(systemId = 'data.vdjserver.org',
-					 filePath = vdjpy.build_vdj_path(uuid, args.file_name, ''), 
+					 filePath = vdjpy.build_vdj_path(uuid, args.file_name, jobfile_boolean, extra_path), 
 					 body = {'action': 'rename', 'path': args.new_name})
     metadata_update = my_agave.meta.updateMetadata(uuid = file_metadata['uuid'], 
 						   body = json.dumps(file_metadata))
