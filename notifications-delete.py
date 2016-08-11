37f7eb60b9429722291d96c3b4aa330db35db1c4
Jacquelyn Turcinovic <jturcinovic@tacc.utexas.edu>
2016-08-11 17:30:32 -0500

    not working; no error

---
+++ notifications-delete.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import vdjpy
+import argparse
+import json
+import os.path
+import urllib
+import sys
+
+if __name__ == '__main__':
+
+    # arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', '--notification_uuid', dest = 'notification_uuid', default = None, nargs = '?')
+    parser.add_argument('-z', '--accesstoken', dest = 'accesstoken', default = None, nargs = '?')
+    parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'store_true')
+    args = parser.parse_args()
+
+    # make Agave object and kwargs
+    my_agave = vdjpy.make_vdj_agave(args.accesstoken)
+    kwargs = {}
+
+    # -a
+    if args.notification_uuid is None:
+        args.notification_uuid = vdjpy.prompt_user('notification uuid')
+    kwargs['uuid'] = args.notification_uuid
+
+    # delete notifications
+    notification_delete = my_agave.notifications.delete(**kwargs)
+
+    # deliver message
+    if notification_delete is None:
+        print 'Successfully deleted notification', args.notification_uuid
+    else:
+        print 'Deletion was not successfull. The message returned from the request was:\n' + json.dumps(notification_delete, default = vdjpy.json_serial, sort_keys = True, indent = 4, separators = (',', ': '))
